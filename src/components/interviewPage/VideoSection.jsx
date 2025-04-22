84c37177807e0db379f0628cae2ebb3c0e8d7ca1
Kauã Kelvyn <ckaua504@gmail.com>
2025-04-22 20:19:55 -0300

    ajustando tamanho do video e foto do entrevistador

--- src/components/interviewPage/VideoSection.jsx
+++ src/components/interviewPage/VideoSection.jsx
@@ -1,22 +1,22 @@
 const VideoSection = ({ videoRef }) => (
   <div className="flex-1 flex items-center justify-center gap-4">
-    <div className="flex flex-col items-center bg-gray-800 rounded-lg p-4 w-1/3">
+    <div className="flex flex-col items-center relative bg-gray-800 rounded-lg w-1/2 h-1/2">
       <img
         src="/entrevistador.jpg"
         alt="Entrevistador"
-        className="w-32 h-32 rounded-full border-2 border-blue-500"
+        className="w-full h-full"
       />
-      <span className="mt-2 text-white">Entrevistador</span>
+      <span className="p-1.5 bg-neutral-800 text-white absolute bottom-0 left-0">Entrevistador</span>
     </div>
     
-    <div className="flex flex-col items-center bg-gray-800 rounded-lg p-4 w-1/3">
+    <div className="flex flex-col items-center relative bg-gray-800 rounded-lg w-1/2 h-1/2">
       <video
         ref={videoRef}
         autoPlay
         playsInline
-        className="w-32 h-32 bg-black rounded-lg"
+        className="w-full h-full bg-black rounded-lg"
       />
-      <span className="mt-2 text-white">
+      <span className="p-1.5 bg-neutral-800 text-white absolute bottom-0 left-0">
         {localStorage.getItem("usuarioNome") || "Você"}
       </span>
     </div>
