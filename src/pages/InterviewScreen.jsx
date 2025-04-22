4e743b87d38bc223aa835b0db31e13b63228fffd
Kauã Kelvyn <ckaua504@gmail.com>
2025-04-22 11:47:03 -0300

    adiconando layout da tela de entrevista

--- src/pages/InterviewScreen.jsx
+++ src/pages/InterviewScreen.jsx
@@ -1,14 +1,72 @@
+import {
+  ShieldCheck,
+  Grid3x3,
+  Mic,
+  MicOff,
+  Camera,
+  CameraOff,
+  MessageSquare,
+} from "lucide-react";
+
 const InterviewScreen = () => {
-    return (
-      <div className="max-w-4xl mx-auto">
-        <div className="bg-white rounded-lg shadow-lg p-6">
-          <h2 className="text-2xl font-bold mb-4">Sala de Entrevista</h2>
-          <div className="border-2 border-dashed border-gray-300 h-96 rounded-lg flex items-center justify-center">
-            <p className="text-gray-500">Interface de entrevista será implementada aqui</p>
+  return (
+    <div className="h-screen w-screen flex flex-col justify-between bg-neutral-900">
+      <header className="flex items-center justify-between p-2">
+        <div className="flex gap-1">
+          <ShieldCheck className="text-blue-500" />
+          <span className="font-semibold text-white">Sala segura, oxe!</span>
+        </div>
+        <button className="flex justify-center items-center rounded-lg gap-1 text-white bg-blue-700 p-1.5 hover:bg-blue-800">
+          <Grid3x3 size={20} />
+          View
+        </button>
+      </header>
+      <div className="flex-1 flex justify-between rounded-lg shadow-lg">
+        <div className="flex-1 rounded-lg gap-1 p-2 flex items-center justify-center">
+          <div className="w-1/2 h-1/2 flex flex-col items-center justify-between bg-amber-100">
+            foto do entrevistador
+            <footer className="w-full">
+              <span className="bg-neutral-800 p-1 text-white">
+                nome do entrevistador
+              </span>
+            </footer>
+          </div>
+          <div className="w-1/2 h-1/2 flex flex-col items-center justify-between bg-amber-100">
+            camera do candidato
+            <footer className="w-full">
+              <span className="bg-neutral-800 p-1 text-white">
+                nome do candidato
+              </span>
+            </footer>
           </div>
         </div>
+        <div className="w-1/5 border-2 border-dashed border-gray-300 text-amber-50">
+          chat
+        </div>
       </div>
-    );
-  };
-  
-  export default InterviewScreen;
+      <footer className="relative bg-blue-950 flex justify-center items-center text-white p-2">
+        <div className="flex gap-5">
+          <button className="flex flex-col items-center">
+            <Mic />
+            <span>Fone</span>
+            {/* <span>Mutado</span> se o microfone for desativado */}
+            {/* <MicOff /> se o microfone for desativado  */}
+          </button>
+          <button className="flex flex-col items-center">
+            <Camera />
+            <span>Câmera</span>
+            {/* <span>Ligar Cam.</span> se a camera for desligada  */}
+            {/* <CameraOff /> se a camera for desligada*/}
+          </button>
+        </div>
+
+        <button className="flex flex-col items-center absolute right-3">
+          <MessageSquare />
+          <span>chat</span>
+        </button>
+      </footer>
+    </div>
+  );
+};
+
+export default InterviewScreen;
